8980ad3432c434029a5edc9f0ade135647d96f9a
panyi <525647740@qq.com>
2016-03-18 19:30:35 +0800

    SearchView AppBar显示

--- app/src/main/java/com/xinlan/toolbaraction/ChildActivity.java
+++ app/src/main/java/com/xinlan/toolbaraction/ChildActivity.java
@@ -2,7 +2,9 @@ package com.xinlan.toolbaraction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -41,19 +43,33 @@ public class ChildActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
+        inflater.inflate(R.menu.sub_menu, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setTranslationX(200);
+
+        MenuItemCompat.setOnActionExpandListener(menuItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                System.out.println("item  expand");
+                return true;  // Return true to expand action view;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                System.out.println("item  collapse");
+                return true;   // Return true to expand action view;
+            }
+        });
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-            case R.id.action_favorite:
-                Intent it = new Intent(this, ChildActivity.class);
-                startActivity(it);
-                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }//end switch
